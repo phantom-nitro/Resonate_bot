891eba8360fd3f0159ae07a22f4c8882e767f528
Goutham Natarajan <39275417+Gouthamcric@users.noreply.github.com>
2021-01-28 08:36:49 +0530

    28-01

--- tests/test_bot.py
+++ tests/test_bot.py
@@ -1,22 +1,38 @@
 # dated : 27th jan
 
 import discord
+import random
+from random import choice
+from discord.ext import commands, tasks
+from PIL import Image,ImageFilter
+from io import BytesIO
 
 #client: bot variable,bot accessed with this.
-client = discord.Client()
+client = commands.Bot(command_prefix=';')
+status = ['Music', 'Movie', 'Surangani Bad Words Remix', 'Adichi Thooku Bad Words Remix', 'Rocket League', 'Rainbow Six Siege','Valorant', 'Slapping Hydrogene', 'VJ mulla', 'Ankan PRO', 'Prime Leader Sudeep', 'Sao Sao_/\_', 'Get Burgified']
 
 @client.event #allows to do certain action when certain event occurs
 async def on_ready():
+    change_status.start()
     general_channel = client.get_channel(759482792265908238)
-    await general_channel.send('salam_Alaikum!')
+    #await general_channel.send('up nd runnin..!')
     #wait for channel to be retrived and then message can be sent(as it is running async.)
 
-client.run('ODA0MDEzNjQzNjQ4OTkxMjUy.YBGKDg.k3e0j0FmWktzP8shgmhI-fStekk')
+@tasks.loop(seconds = 20)
+async def change_status():
+    await client.change_presence(activity=discord.Game(choice(status)))
 
+@client.command()
+async def rip(ctx,member:discord.Member=None):
+    if not member:
+        member =  ctx.author
 
+    rip = Image.open('rip.png')
+    asset = member.avatar_url_as(size=128)
+    data = BytesIO(await asset.read())
+    pfp = Image.open(data)
 
-
-#----------------------------
-#804013643648991252 - bot id
-#308b39f48942607ff24c08de04164ae2d44d05e399aa3dc7a1dcb846c629fa39 -public key
-#ODA0MDEzNjQzNjQ4OTkxMjUy.YBGKDg.k3e0j0FmWktzP8shgmhI-fStekk -token
+    pfp = pfp.resize((257,187))
+    rip.paste(pfp,(67,258))
+    rip.save('prip.png')
+    await ctx.send(file = discord.File('prip.png'))
