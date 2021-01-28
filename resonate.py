6a4bc4c95ba19e4d5745e4a090d43200e9f8e20f
phantom-nitro <57362323+phantom-nitro@users.noreply.github.com>
2021-01-28 19:28:05 +0530

    Update resonate.py

--- resonate.py
+++ resonate.py
@@ -78,10 +78,14 @@ async def d(ctx, *, msg = 26):
 	inputnumber = msg
 	iteration = 2*(inputnumber-1)
 	alphabets = list(string.ascii_lowercase)
+	em = discord.Embed(title = "+", color = discord.Color.red())
 	a1 = [*alphabets[1:inputnumber][::-1]]
 	a2 = [*alphabets[0:inputnumber]]
 	k = a1 + a2
-	await ctx.send(f"{k}")
+	em.add_field(name = "\u200b", value = k, inline = False)
+	#k = a1 + a2
+	#await ctx.send(f"{k}")
+	await ctx.send(embed = em)
 	
 
 @client.command(aliases = ["bal"])
