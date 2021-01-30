fe01d57f688eb525bf5430539139f17f6e33a585
phantom-nitro <57362323+phantom-nitro@users.noreply.github.com>
2021-01-30 22:48:42 +0530

    Update resonate.py

--- resonate.py
+++ resonate.py
@@ -194,7 +194,7 @@ async def ping(ctx):
 	await ctx.send(f':zap: Latency: {round(client.latency*1000)}ms')
 	
 @client.command(pass_context = True)
-async def echo(ctx, n = 1, *,  msg):
+async def echo(ctx, msg, n: Optional[int]):
 	if n > 100:
 		await ctx.send('echo should not be greater than 100')
 	else:
