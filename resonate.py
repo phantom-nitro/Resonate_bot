4c8d39db91e45857c76090b794fd5eaa281a220a
Goutham Natarajan <39275417+Gouthamcric@users.noreply.github.com>
2021-03-26 13:53:03 +0530

    Update resonate.py

--- resonate.py
+++ resonate.py
@@ -16,6 +16,7 @@ import pymongo
 import dns
 import praw
 import asyncio
+import requests
 reddit = praw.Reddit(client_id = "Vaaq3s4VInOk8Q",
                     client_secret = "YgPXbnCvKUtvJ3eNbk07JjfkkGGIrw",
                     username = "tempgoutham",
@@ -58,7 +59,44 @@ async def postmeme(ctx):
         await ctx.send(embed = em)
         await asyncio.sleep(1800)
 
+@client.command(name = 'covid')
+async def covid2(ctx,countryName):
+    if countryName is None:
+                embed=discord.Embed(title="This command is used like this: ```;covid [country]```", colour=0xff0000, timestamp=ctx.message.created_at)
+                await ctx.send(embed=embed)
+    else:
+                url = f"https://coronavirus-19-api.herokuapp.com/countries/{countryName}"
+                stats = requests.get(url)
+                json_stats = stats.json()
+                country = json_stats["country"]
+                totalCases = json_stats["cases"]
+                todayCases = json_stats["todayCases"]
+                totalDeaths = json_stats["deaths"]
+                todayDeaths = json_stats["todayDeaths"]
+                recovered = json_stats["recovered"]
+                active = json_stats["active"]
+                critical = json_stats["critical"]
+                casesPerOneMillion = json_stats["casesPerOneMillion"]
+                deathsPerOneMillion = json_stats["deathsPerOneMillion"]
+                totalTests = json_stats["totalTests"]
+                testsPerOneMillion = json_stats["testsPerOneMillion"]
 
+                embed2 = discord.Embed(title=f"**COVID-19 Status Of {country}**!", description="This Information Isn't Live Always, Hence It May Not Be Accurate!", colour=0x0000ff, timestamp=ctx.message.created_at)
+                embed2.add_field(name="**Total Cases**", value=totalCases, inline=True)
+                embed2.add_field(name="**Today Cases**", value=todayCases, inline=True)
+                embed2.add_field(name="**Total Deaths**", value=totalDeaths, inline=True)
+                embed2.add_field(name="**Today Deaths**", value=todayDeaths, inline=True)
+                embed2.add_field(name="**Recovered**", value=recovered, inline=True)
+                embed2.add_field(name="**Active**", value=active, inline=True)
+                embed2.add_field(name="**Critical**", value=critical, inline=True)
+                embed2.add_field(name="**Cases Per One Million**", value=casesPerOneMillion, inline=True)
+                embed2.add_field(name="**Deaths Per One Million**", value=deathsPerOneMillion, inline=True)
+                embed2.add_field(name="**Total Tests**", value=totalTests, inline=True)
+                embed2.add_field(name="**Tests Per One Million**", value=testsPerOneMillion, inline=True)
+
+                embed2.set_thumbnail(url="https://cdn.discordapp.com/attachments/564520348821749766/701422183217365052/2Q.png")
+                await ctx.send(embed=embed2)
+		
 @client.command()
 async def meme(ctx):
     subreddit = reddit.subreddit("memes")
