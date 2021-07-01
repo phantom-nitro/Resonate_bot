86e180082eb1ce15641e1c839b4fdfcf84902d7b
phantom-nitro <57362323+phantom-nitro@users.noreply.github.com>
2021-07-01 10:23:11 +0530

    Update music.py

--- music.py
+++ music.py
@@ -3,8 +3,23 @@ from discord.ext import commands
 import youtube_dl
 
 class music(commands.Cog):
-    def _init_(self, client):
-        self.client = client
+	def _init_(self, client):
+		self.client = client
+	
+	@commands.command()
+	async def join(self, ctx):
+		if ctx.author.voice is None:
+			await.ctx.send("Join any Voice Channel")
+		voice_channel = ctx.author.voice.channel
+		if ctx.voice_client is None:
+			await voice_channel.connect()
+		else:
+			ctx.voice_client.move_to(voice_channel)
+			
+	@commands.command()
+	async def disconnect(self, ctx):
+		await ctx.voice_client.disconnect()
       
 def setup(client):
-    client.add_cog(music(client)
+	client.add_cog(music(client)
+
