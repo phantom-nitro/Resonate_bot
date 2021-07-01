a8a80834802d2f7f8866efe909599b9102de3062
phantom-nitro <57362323+phantom-nitro@users.noreply.github.com>
2021-07-01 10:15:47 +0530

    Update music.py

--- music.py
+++ music.py
@@ -1,1 +1,10 @@
+import discord
+from discord.ext import commands
+import youtube_dl
 
+class music(commands.Cog):
+    def _init_(self, client):
+        self.client = client
+      
+def setup(client):
+    client.add_cog(music(client)
